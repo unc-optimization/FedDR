8cec92824adeab477931bbc60addcb48fdbe71ab
Nhan Pham <nhpham@Nhans-MBP-2.nc.rr.com>
2022-03-08 15:53:00 -0500

    fix prox_l2_norm_square and add prox_l2_norm

--- FedDR/flearn/regularizers/__init__.py
+++ FedDR/flearn/regularizers/__init__.py
@@ -1,5 +1,6 @@
 from .l1_norm import L1_Norm
 from .l2_norm_squared import L2_Norm_Squared
+from .l2_norm import L2_Norm
 from .linf_norm import LInf_Norm
 from .empty_func import Empty_Function
 
@@ -7,5 +8,6 @@ REGISTRY = {}
 
 REGISTRY["l1_norm"] = L1_Norm
 REGISTRY["l2_norm_squared"] = L2_Norm_Squared
+REGISTRY["l2_norm"] = L2_Norm
 REGISTRY["linf_norm"] = LInf_Norm
 REGISTRY["none"] = Empty_Function
--- FedDR/flearn/regularizers/l2_norm.py
+++ FedDR/flearn/regularizers/l2_norm.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class L2_Norm_Squared(object):
+class L2_Norm(object):
     def __init__(self, coeff=1.):
         self.coeff = coeff
 
@@ -20,7 +20,7 @@ class L2_Norm_Squared(object):
     def prox_eval(self, x, prox_param):
         """! Compute the proximal operator of the \f$\ell_2^2\f$ - norm
 
-        \f$ prox_{\lambda \|.\|_2^2} = {arg\min_z}\left\{\|.\|_2^2 + \frac{1}{2\lambda}\|z - x\|^2\right\} \f$
+        \f$ prox_{\lambda \|.\|_2^2} = {arg\min_z}\left\{\|.\|_2^2 + \|z - x\|_2\right\} \f$
         
         Parameters
         ---------- 
@@ -32,4 +32,5 @@ class L2_Norm_Squared(object):
         @retval : output vector
         """
         prox_param *= self.coeff
-        return (1./(1+ prox_param)) * x
+        norm_x = np.linalg.norm(x, ord=2)
+        return np.maximum(1 - prox_param/norm_x, 0) * x
